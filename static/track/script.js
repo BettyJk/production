30036d545a28fa6e5607a5a9182ada93f2b4ce26
BettyJK <jouakboutayna2003@gmail.com>
2024-07-10 16:53:12 +0100

    Initial commit

---
+++ static/track/script.js
@@ -0,0 +1,8 @@
+function navigate(direction) {
+    // Implement navigation logic here
+    alert('Navigation button clicked: ' + direction);
+}
+
+function exitPage() {
+    window.location.href = '/'; // Redirect to welcome page or desired exit URL
+}
